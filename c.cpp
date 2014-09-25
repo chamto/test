63781af112f1196f1b95e91df93ec7b503b51053
testChamto <anrudco@gmail.com>
2014-09-25 20:30:03 +0900

    물음 파일 추가

---
+++ c.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str = "";
+    std::cout << "무엇을 하느냐 그것이 문제로다 :  " ;
+    std::cin >> str;
+    std::cout << std::endl;
+    std::cout << str;
+    std::cout << std::endl;
+
+    return 0;
+}
